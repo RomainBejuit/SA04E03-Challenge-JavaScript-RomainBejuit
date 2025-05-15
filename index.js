13854fefd22e0349eacd18a3f1a234aa6b7f0390
RomainBejuit <romain_du38@hotmail.fr>
2025-05-15 14:27:48 +0200

    exo aprem 15/05

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*
+Challenge 14/05 :
+
 const itemList = ["souris", "écran", "clavier", "ordinateur portable", "tapis de souris", "casque", "câble USB", "téléphone portable"];
 
 console.log("Bonjour ! Les objets disponibles sont :");
@@ -10,3 +13,23 @@ const recherche = prompt("Recherchez vos articles :");
 
 console.log(
     `L'article ${recherche} ${itemList.includes(recherche) ? "est" : "n'est pas" } dans la liste`);
+
+
+const results = [];
+
+
+
+Exo 15/05 : 
+
+const france = {
+    plusGrandesVilles: ["Paris", "Marseille", "Lyon"],
+    population: 68000000,
+    capitale: {name: "Paris", population: 10890751},
+}
+france.population = 68373433;
+console.log(france);
+
+console.log("La troisième plus grande ville de France est " + france.capitale.name + " et la population de la capitale est " + france.capitale.population);
+
+*/
+
